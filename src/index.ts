1a486252f3151747a3541e20cdd8ed7270d4116a
Hexona <xuanjiap@lolicon.ac.cn>
2023-11-30 10:59:22 -0500

    feat: update descriptions

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import Kasumi, { BaseCommand, BaseSession, Card, CommandFunction } from "kasumi.js";
 
 class PingCommand extends BaseCommand<Kasumi<any>> {
+    description = "测试 Kasumi 与 KOOK 服务器之间的延迟。"
+
     constructor(name: string = "ping") {
         super();
         this.name = name;
@@ -75,13 +77,13 @@ class PingCommand extends BaseCommand<Kasumi<any>> {
                             })
                             .addDivider()
                             .addText(`Local Latency: ${colorizeLatencyString(localLatency)}`)
-                            .addContext("Difference between timestamp from message event and local timestamp.\nRepresents the time it takes to complete a `/message/create` API call.")
+                            .addContext("The time it takes to complete a /message/create API call.")
                             .addText(`Remote Latency: ${colorizeLatencyString(remoteLatency)}`)
-                            .addContext("Difference between local timestamp and remote timestamp from API response.\nGenerally represents the time it takes to finish a barebone command like this.")
+                            .addContext("The time it takes to finish a barebone command like this.")
                             .addText(`Origin Diff: ${colorizeLatencyString(originDiff)}`)
-                            .addContext("Difference between local timestamp when recieved message event and local timestamp after sending initial message.\nGenerally represents the time it takes for KOOK to deliver the message event to client, but take account for time inaccuracy on both servers.")
+                            .addContext("The time it takes for KOOK to deliver the message event to client, but take account for time inaccuracy on both servers.")
                             .addText(`Response Diff: ${colorizeLatencyString(responseDiff)}`)
-                            .addContext("Difference between timestamp from message event and timestamp from API response.\n*Unreliably* represents the time it takes for KOOK to process a `/message/create` API call.")
+                            .addContext("**Unreliable** The time it takes for KOOK to process a `/message/create` API call.")
                         ])
                     }
                 }
